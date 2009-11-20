2b602b611cefcaf02fc33bd4da7974582bb401e9
davie3 <davie3@76012f3b-c22a-4fc1-af2a-f3da97e549b2>
2009-11-20 07:19:12 +0000

    Tagging as v1.0.1

---
+++ tags/v1.0.1/Data/Defaults.lua
@@ -0,0 +1,14 @@
+﻿local WhoTaunted = WhoTaunted;
+
+WhoTaunted.defaults = {
+	profile = {
+		Disabled = false,
+		DisableInBG = true,
+		AnounceTaunts = true,
+		AnounceTauntsOutput = 1,
+		AnounceAOETaunts = true,
+		AnounceAOETauntsOutput = 1,
+		AnounceFails = true,
+		AnounceFailsOutput = 1,
+	},
+}
